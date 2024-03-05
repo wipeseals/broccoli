6269889844504766dc8088fa5eaaca7945a69f9e
wipeseals <wipeseals@gmail.com>
2024-03-05 23:54:33 +0900

    test(wip): pio sample動作確認OK

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,13 @@
 
 mod nandio;
 
-use bsp::entry;
+use bsp::{
+    entry,
+    hal::{
+        gpio::{FunctionPio0, Pin},
+        pio::{PIOBuilder, PIOExt},
+    },
+};
 use defmt::*;
 use defmt_rtt as _;
 use embedded_hal::digital::v2::{InputPin, OutputPin};
@@ -17,6 +23,8 @@ use bsp::hal::{
     sio::Sio,
     watchdog::Watchdog,
 };
+
+use pio_proc::pio_asm;
 
 #[entry]
 fn main() -> ! {
@@ -50,191 +58,212 @@ fn main() -> ! {
         &mut pac.RESETS,
     );
     // gpio25
-    let mut led_pin = pins.led.into_push_pull_output();
+    let mut led_pin: Pin<_, FunctionPio0, _> = pins.led.into_function();
 
     delay.delay_ms(1000);
-    info!("test ID Read");
+    info!("== broccoli ==");
 
-    //////////////////////////
-    // pin assign & init
-    // IO: I/O Port 0~7
-    let mut io0_pin = pins
-        .gpio0
-        .into_push_pull_output_in_state(bsp::hal::gpio::PinState::Low);
-    let mut io1_pin = pins
-        .gpio1
-        .into_push_pull_output_in_state(bsp::hal::gpio::PinState::Low);
-    let mut io2_pin = pins
-        .gpio2
-        .into_push_pull_output_in_state(bsp::hal::gpio::PinState::Low);
-    let mut io3_pin = pins
-        .gpio3
-        .into_push_pull_output_in_state(bsp::hal::gpio::PinState::Low);
-    let mut io4_pin = pins
-        .gpio4
-        .into_push_pull_output_in_state(bsp::hal::gpio::PinState::Low);
-    let mut io5_pin = pins
-        .gpio5
-        .into_push_pull_output_in_state(bsp::hal::gpio::PinState::Low);
-    let mut io6_pin = pins
-        .gpio6
-        .into_push_pull_output_in_state(bsp::hal::gpio::PinState::Low);
-    let mut io7_pin = pins
-        .gpio7
-        .into_push_pull_output_in_state(bsp::hal::gpio::PinState::Low);
-    // /CE: Chip Enable 0
-    let mut ceb0_pin = pins
-        .gpio8
-        .into_push_pull_output_in_state(bsp::hal::gpio::PinState::High);
-    // /CE: Chip Enable 1
-    let mut ceb1_pin = pins
-        .gpio9
-        .into_push_pull_output_in_state(bsp::hal::gpio::PinState::High);
-    // CLE: Command Latch Enable
-    let mut cle_pin = pins
-        .gpio10
-        .into_push_pull_output_in_state(bsp::hal::gpio::PinState::Low);
-    // ALE: Address Latch Enable
-    let mut ale_pin = pins
-        .gpio11
-        .into_push_pull_output_in_state(bsp::hal::gpio::PinState::Low);
-    // /WP: Write Protect
-    let _wpb_pin = pins
-        .gpio12
-        .into_push_pull_output_in_state(bsp::hal::gpio::PinState::Low);
-    // /WE: Write Enable
-    let mut web_pin = pins
-        .gpio13
-        .into_push_pull_output_in_state(bsp::hal::gpio::PinState::High);
-    // /RE: Read Enable
-    let mut reb_pin = pins
-        .gpio14
-        .into_push_pull_output_in_state(bsp::hal::gpio::PinState::High);
-    // RY / /BY: Read/Busy
-    let _rbb_pin = pins.gpio15.into_pull_up_input();
-    delay.delay_ms(1);
+    // pio program
+    const MAX_DELAY: u8 = 31;
+    let mut assembler = pio::Assembler::<32>::new();
+    let mut wrap_target = assembler.label();
+    let mut wrap_source = assembler.label();
+    assembler.set(pio::SetDestination::PINDIRS, 1);
+    assembler.bind(&mut wrap_target);
+    assembler.set_with_delay(pio::SetDestination::PINS, 0, MAX_DELAY);
+    assembler.set_with_delay(pio::SetDestination::PINS, 1, MAX_DELAY);
+    assembler.bind(&mut wrap_source);
+    let program = assembler.assemble_with_wrap(wrap_source, wrap_target);
 
-    ////////////////////////////////
-    // command latch 0x90 (ID Read)
-    io0_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
-    io1_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
-    io2_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
-    io3_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
-    io4_pin.set_state(bsp::hal::gpio::PinState::High).unwrap();
-    io5_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
-    io6_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
-    io7_pin.set_state(bsp::hal::gpio::PinState::High).unwrap();
+    // run pio0
+    let (mut pio, sm0, _, _, _) = pac.PIO0.split(&mut pac.RESETS);
+    let installed = pio.install(&program).unwrap();
+    let (int, frac) = (0, 0);
+    let (sm, _, _) = PIOBuilder::from_program(installed)
+        .set_pins(led_pin.id().num, 1)
+        .clock_divisor_fixed_point(int, frac)
+        .build(sm0);
+    sm.start();
+    // //////////////////////////
+    // // pin assign & init
+    // // IO: I/O Port 0~7
+    // let mut io0_pin = pins
+    //     .gpio0
+    //     .into_push_pull_output_in_state(bsp::hal::gpio::PinState::Low);
+    // let mut io1_pin = pins
+    //     .gpio1
+    //     .into_push_pull_output_in_state(bsp::hal::gpio::PinState::Low);
+    // let mut io2_pin = pins
+    //     .gpio2
+    //     .into_push_pull_output_in_state(bsp::hal::gpio::PinState::Low);
+    // let mut io3_pin = pins
+    //     .gpio3
+    //     .into_push_pull_output_in_state(bsp::hal::gpio::PinState::Low);
+    // let mut io4_pin = pins
+    //     .gpio4
+    //     .into_push_pull_output_in_state(bsp::hal::gpio::PinState::Low);
+    // let mut io5_pin = pins
+    //     .gpio5
+    //     .into_push_pull_output_in_state(bsp::hal::gpio::PinState::Low);
+    // let mut io6_pin = pins
+    //     .gpio6
+    //     .into_push_pull_output_in_state(bsp::hal::gpio::PinState::Low);
+    // let mut io7_pin = pins
+    //     .gpio7
+    //     .into_push_pull_output_in_state(bsp::hal::gpio::PinState::Low);
+    // // /CE: Chip Enable 0
+    // let mut ceb0_pin = pins
+    //     .gpio8
+    //     .into_push_pull_output_in_state(bsp::hal::gpio::PinState::High);
+    // // /CE: Chip Enable 1
+    // let mut ceb1_pin = pins
+    //     .gpio9
+    //     .into_push_pull_output_in_state(bsp::hal::gpio::PinState::High);
+    // // CLE: Command Latch Enable
+    // let mut cle_pin = pins
+    //     .gpio10
+    //     .into_push_pull_output_in_state(bsp::hal::gpio::PinState::Low);
+    // // ALE: Address Latch Enable
+    // let mut ale_pin = pins
+    //     .gpio11
+    //     .into_push_pull_output_in_state(bsp::hal::gpio::PinState::Low);
+    // // /WP: Write Protect
+    // let _wpb_pin = pins
+    //     .gpio12
+    //     .into_push_pull_output_in_state(bsp::hal::gpio::PinState::Low);
+    // // /WE: Write Enable
+    // let mut web_pin = pins
+    //     .gpio13
+    //     .into_push_pull_output_in_state(bsp::hal::gpio::PinState::High);
+    // // /RE: Read Enable
+    // let mut reb_pin = pins
+    //     .gpio14
+    //     .into_push_pull_output_in_state(bsp::hal::gpio::PinState::High);
+    // // RY / /BY: Read/Busy
+    // let _rbb_pin = pins.gpio15.into_pull_up_input();
+    // delay.delay_ms(1);
 
-    cle_pin.set_state(bsp::hal::gpio::PinState::High).unwrap();
+    // ////////////////////////////////
+    // // command latch 0x90 (ID Read)
+    // io0_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
+    // io1_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
+    // io2_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
+    // io3_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
+    // io4_pin.set_state(bsp::hal::gpio::PinState::High).unwrap();
+    // io5_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
+    // io6_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
+    // io7_pin.set_state(bsp::hal::gpio::PinState::High).unwrap();
 
-    ceb0_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
-    ceb1_pin.set_state(bsp::hal::gpio::PinState::High).unwrap();
+    // cle_pin.set_state(bsp::hal::gpio::PinState::High).unwrap();
 
-    web_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
-    ale_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
-    reb_pin.set_state(bsp::hal::gpio::PinState::High).unwrap();
-    delay.delay_ms(1);
+    // ceb0_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
+    // ceb1_pin.set_state(bsp::hal::gpio::PinState::High).unwrap();
 
-    web_pin.set_state(bsp::hal::gpio::PinState::High).unwrap();
-    delay.delay_ms(1);
+    // web_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
+    // ale_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
+    // reb_pin.set_state(bsp::hal::gpio::PinState::High).unwrap();
+    // delay.delay_ms(1);
 
-    ////////////////////////////////
-    // Address latch 0x00
-    io0_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
-    io1_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
-    io2_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
-    io3_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
-    io4_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
-    io5_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
-    io6_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
-    io7_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
+    // web_pin.set_state(bsp::hal::gpio::PinState::High).unwrap();
+    // delay.delay_ms(1);
 
-    cle_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
+    // ////////////////////////////////
+    // // Address latch 0x00
+    // io0_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
+    // io1_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
+    // io2_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
+    // io3_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
+    // io4_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
+    // io5_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
+    // io6_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
+    // io7_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
 
-    ceb0_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
-    ceb1_pin.set_state(bsp::hal::gpio::PinState::High).unwrap();
+    // cle_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
 
-    web_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
-    ale_pin.set_state(bsp::hal::gpio::PinState::High).unwrap();
-    reb_pin.set_state(bsp::hal::gpio::PinState::High).unwrap();
-    delay.delay_ms(1);
+    // ceb0_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
+    // ceb1_pin.set_state(bsp::hal::gpio::PinState::High).unwrap();
 
-    web_pin.set_state(bsp::hal::gpio::PinState::High).unwrap();
-    delay.delay_ms(1); // TODO: wait t_AR
+    // web_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
+    // ale_pin.set_state(bsp::hal::gpio::PinState::High).unwrap();
+    // reb_pin.set_state(bsp::hal::gpio::PinState::High).unwrap();
+    // delay.delay_ms(1);
 
-    ////////////////////////////////
-    // ready for dataread (/RE = Low & IO dir = Input)
-    let io0_pin = io0_pin.into_pull_down_input();
-    let io1_pin = io1_pin.into_pull_down_input();
-    let io2_pin = io2_pin.into_pull_down_input();
-    let io3_pin = io3_pin.into_pull_down_input();
-    let io4_pin = io4_pin.into_pull_down_input();
-    let io5_pin = io5_pin.into_pull_down_input();
-    let io6_pin = io6_pin.into_pull_down_input();
-    let io7_pin = io7_pin.into_pull_down_input();
+    // web_pin.set_state(bsp::hal::gpio::PinState::High).unwrap();
+    // delay.delay_ms(1); // TODO: wait t_AR
 
-    cle_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
+    // ////////////////////////////////
+    // // ready for dataread (/RE = Low & IO dir = Input)
+    // let io0_pin = io0_pin.into_pull_down_input();
+    // let io1_pin = io1_pin.into_pull_down_input();
+    // let io2_pin = io2_pin.into_pull_down_input();
+    // let io3_pin = io3_pin.into_pull_down_input();
+    // let io4_pin = io4_pin.into_pull_down_input();
+    // let io5_pin = io5_pin.into_pull_down_input();
+    // let io6_pin = io6_pin.into_pull_down_input();
+    // let io7_pin = io7_pin.into_pull_down_input();
 
-    ceb0_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
-    ceb1_pin.set_state(bsp::hal::gpio::PinState::High).unwrap();
+    // cle_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
 
-    web_pin.set_state(bsp::hal::gpio::PinState::High).unwrap();
-    ale_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
-    reb_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
-    delay.delay_ms(1); // TODO: wait t_REA
+    // ceb0_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
+    // ceb1_pin.set_state(bsp::hal::gpio::PinState::High).unwrap();
 
-    //////////////////////////////////////
-    // data read
-    for read_index in 0..5 {
-        let read_data: u8 = {
-            (if io0_pin.is_high().unwrap() {
-                0x01
-            } else {
-                0x00
-            }) | (if io1_pin.is_high().unwrap() {
-                0x02
-            } else {
-                0x00
-            }) | (if io2_pin.is_high().unwrap() {
-                0x04
-            } else {
-                0x00
-            }) | (if io3_pin.is_high().unwrap() {
-                0x08
-            } else {
-                0x00
-            }) | (if io4_pin.is_high().unwrap() {
-                0x10
-            } else {
-                0x00
-            }) | (if io5_pin.is_high().unwrap() {
-                0x20
-            } else {
-                0x00
-            }) | (if io6_pin.is_high().unwrap() {
-                0x40
-            } else {
-                0x00
-            }) | (if io7_pin.is_high().unwrap() {
-                0x80
-            } else {
-                0x00
-            })
-        };
-        info!("data[{}] = {:#02x}", read_index, read_data);
+    // web_pin.set_state(bsp::hal::gpio::PinState::High).unwrap();
+    // ale_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
+    // reb_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
+    // delay.delay_ms(1); // TODO: wait t_REA
 
-        // next cyc
-        reb_pin.set_state(bsp::hal::gpio::PinState::High).unwrap();
-        delay.delay_ms(1);
-        reb_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
-        delay.delay_ms(1); // TODO: wait t_REA
-    }
+    // //////////////////////////////////////
+    // // data read
+    // for read_index in 0..5 {
+    //     let read_data: u8 = {
+    //         (if io0_pin.is_high().unwrap() {
+    //             0x01
+    //         } else {
+    //             0x00
+    //         }) | (if io1_pin.is_high().unwrap() {
+    //             0x02
+    //         } else {
+    //             0x00
+    //         }) | (if io2_pin.is_high().unwrap() {
+    //             0x04
+    //         } else {
+    //             0x00
+    //         }) | (if io3_pin.is_high().unwrap() {
+    //             0x08
+    //         } else {
+    //             0x00
+    //         }) | (if io4_pin.is_high().unwrap() {
+    //             0x10
+    //         } else {
+    //             0x00
+    //         }) | (if io5_pin.is_high().unwrap() {
+    //             0x20
+    //         } else {
+    //             0x00
+    //         }) | (if io6_pin.is_high().unwrap() {
+    //             0x40
+    //         } else {
+    //             0x00
+    //         }) | (if io7_pin.is_high().unwrap() {
+    //             0x80
+    //         } else {
+    //             0x00
+    //         })
+    //     };
+    //     info!("data[{}] = {:#02x}", read_index, read_data);
+
+    //     // next cyc
+    //     reb_pin.set_state(bsp::hal::gpio::PinState::High).unwrap();
+    //     delay.delay_ms(1);
+    //     reb_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
+    //     delay.delay_ms(1); // TODO: wait t_REA
+    // }
 
     loop {
-        led_pin.set_high().unwrap();
-        delay.delay_ms(1000);
-        led_pin.set_low().unwrap();
-        delay.delay_ms(1000);
+        // led_pin.set_high().unwrap();
+        // delay.delay_ms(1000);
+        // led_pin.set_low().unwrap();
+        // delay.delay_ms(1000);
     }
 }
 
