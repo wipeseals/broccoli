a763f6199d2137b484bd7aa546e6255bcd1b3502
wipeseals <wipeseals@gmail.com>
2024-04-10 23:33:27 +0900

    revert: main.rsの実装をGPIO動作確認時に戻す

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,12 @@
 
 mod nandio;
 
-use alloc::boxed::Box;
-use bsp::{
-    entry,
-    hal::{
-        gpio::{FunctionPio0, Pin},
-        pio::{PIOBuilder, PIOExt},
-    },
-};
+use bsp::entry;
 use defmt::*;
 use defmt_rtt as _;
 use embedded_hal::digital::v2::{InputPin, OutputPin};
 use panic_probe as _;
 
-use pio::Assembler;
 use rp_pico as bsp;
 
 use bsp::hal::{
@@ -25,113 +17,6 @@ use bsp::hal::{
     sio::Sio,
     watchdog::Watchdog,
 };
-
-use bitflags;
-
-/// TC58NVG0S3HTA00
-/// NAND ICのコマンド定義
-pub struct NandCmdSpec {
-    first: u8,
-    second: Option<u8>,
-    acceptable_while_busy: bool,
-}
-
-/// NAND ICのコマンド列挙
-pub enum NandCmd {
-    SerialDataInput,
-    Read,
-    ColumnAddressChangeInSerialDataOutput,
-    ReadWithDataCache,
-    ReadStartForLastPageInReadCycleWithDataCache,
-    AutoPageProgram,
-    ColumnAddressChangeInSerialDataInput,
-    AutoPageProgramWithDataCache,
-    ReadForPageCopyWithDataOut,
-    AutoPageProgramWithDataCacheDuringPageCopy,
-    AutoPageProgramForLastPageDuringPageCopy,
-    AutoBlockErase,
-    IdRead,
-    Statusread,
-    Reset,
-}
-
-bitflags! {
-    /// StatusReadの結果
-    pub struct StatusReadResult: u8 {
-        /// pass=0, fail=1
-        const CHIP_STATUS_1 = 0b_00000001;
-        /// pass=0, fail=1
-        const CHIP_STATUS_2 = 0b_00000010;
-        const RESERVED_1 = 0b_00000100;
-        const RESERVED_2 = 0b_00001000;
-        const RESERVED_3 = 0b_00010000;
-        /// Page Buffer Busy=0, Page Buffer Ready=1
-        const PAGE_BUFFER_READY_BUSYB = 0b_00100000;
-        /// Data Cache Busy=0, Data Cache Ready=1
-        const DATA_CACHE_READY_BUSYB = 0b_01000000;
-        /// Write Protect Protected=0, Write Protect Not Protected=1
-        const WRITE_PROTECT = 0b_10000000;
-    }
-}
-
-/// NAND IC Interconnect
-/// | 31  | 30  | 29  | 28  | 27  | 26  | 25  | 24  | 23  | 22  | 21  | 20  | 19  | 18  | 17  | 16  | 15  | 14  | 13  | 12  | 11  | 10  | 9    | 8    | 7   | 6   | 5   | 4   | 3   | 2   | 1   | 0   |
-/// | --- | --- | --- | --- | --- | --- | --- | --- | --- | --- | --- | --- | --- | --- | --- | --- | --- | --- | --- | --- | --- | --- | ---- | ---- | --- | --- | --- | --- | --- | --- | --- | --- |
-/// | --  | --  | --  | --  | --  | --  | led | --  | --  | --  | --  | --  | --  | --  | --  | --  | rbb | reb | web | wpb | ale | cle | ceb1 | ceb0 | io7 | io6 | io5 | io4 | io3 | io2 | io1 | io0 |
-#[repr(u32)]
-pub enum NandPinAssign {
-    Io0Pin = 0,
-    Io1Pin = 1,
-    Io2Pin = 2,
-    Io3Pin = 3,
-    Io4Pin = 4,
-    Io5Pin = 5,
-    Io6Pin = 6,
-    Io7Pin = 7,
-    Ceb0Pin = 8,
-    Ceb1Pin = 9,
-    ClePin = 10,
-    AlePin = 11,
-    WpbPin = 12,
-    WebPin = 13,
-    RebPin = 14,
-    RbbPin = 15,
-    LedPin = 25,
-}
-
-/// PIOのコマンド定義
-/// SET Cmdで値を入れるので、コマンドは5bit (0x00~0x1f) の範囲であること
-///
-/// ## Command Description
-///
-///
-/// ## Example
-///
-/// - Reset
-/// - ID read
-/// - StatusRead
-/// - READ
-/// - Program
-/// - Erase
-///
-#[repr(u8)]
-pub enum NandPioCmd {
-    Nop = 0x00,
-    PinDir = 0x01,
-    PinInit = 0x02,
-    CmdLatch = 0x03,
-    AddrLatch = 0x04,
-    WaitRbb = 0x05,
-    WriteData = 0x06,
-    ReadData = 0x07,
-    SendIrq = 0x08,
-}
-
-impl Default for NandPioCmd {
-    fn default() -> Self {
-        Self::Nop
-    }
-}
 
 #[entry]
 fn main() -> ! {
@@ -165,79 +50,191 @@ fn main() -> ! {
         &mut pac.RESETS,
     );
     // gpio25
-    let mut led_pin: Pin<_, FunctionPio0, _> = pins.led.into_function();
+    let mut led_pin = pins.led.into_push_pull_output();
 
     delay.delay_ms(1000);
-    info!("== broccoli ==");
+    info!("test ID Read");
 
-    const IRQ_INDEX: u8 = 0;
+    //////////////////////////
+    // pin assign & init
+    // IO: I/O Port 0~7
+    let mut io0_pin = pins
+        .gpio0
+        .into_push_pull_output_in_state(bsp::hal::gpio::PinState::Low);
+    let mut io1_pin = pins
+        .gpio1
+        .into_push_pull_output_in_state(bsp::hal::gpio::PinState::Low);
+    let mut io2_pin = pins
+        .gpio2
+        .into_push_pull_output_in_state(bsp::hal::gpio::PinState::Low);
+    let mut io3_pin = pins
+        .gpio3
+        .into_push_pull_output_in_state(bsp::hal::gpio::PinState::Low);
+    let mut io4_pin = pins
+        .gpio4
+        .into_push_pull_output_in_state(bsp::hal::gpio::PinState::Low);
+    let mut io5_pin = pins
+        .gpio5
+        .into_push_pull_output_in_state(bsp::hal::gpio::PinState::Low);
+    let mut io6_pin = pins
+        .gpio6
+        .into_push_pull_output_in_state(bsp::hal::gpio::PinState::Low);
+    let mut io7_pin = pins
+        .gpio7
+        .into_push_pull_output_in_state(bsp::hal::gpio::PinState::Low);
+    // /CE: Chip Enable 0
+    let mut ceb0_pin = pins
+        .gpio8
+        .into_push_pull_output_in_state(bsp::hal::gpio::PinState::High);
+    // /CE: Chip Enable 1
+    let mut ceb1_pin = pins
+        .gpio9
+        .into_push_pull_output_in_state(bsp::hal::gpio::PinState::High);
+    // CLE: Command Latch Enable
+    let mut cle_pin = pins
+        .gpio10
+        .into_push_pull_output_in_state(bsp::hal::gpio::PinState::Low);
+    // ALE: Address Latch Enable
+    let mut ale_pin = pins
+        .gpio11
+        .into_push_pull_output_in_state(bsp::hal::gpio::PinState::Low);
+    // /WP: Write Protect
+    let _wpb_pin = pins
+        .gpio12
+        .into_push_pull_output_in_state(bsp::hal::gpio::PinState::Low);
+    // /WE: Write Enable
+    let mut web_pin = pins
+        .gpio13
+        .into_push_pull_output_in_state(bsp::hal::gpio::PinState::High);
+    // /RE: Read Enable
+    let mut reb_pin = pins
+        .gpio14
+        .into_push_pull_output_in_state(bsp::hal::gpio::PinState::High);
+    // RY / /BY: Read/Busy
+    let _rbb_pin = pins.gpio15.into_pull_up_input();
+    delay.delay_ms(1);
 
-    let mut assembler = pio::Assembler::<32>::new();
-    let mut label_wrap_target = assembler.label();
-    let mut label_wrap_source = assembler.label();
+    ////////////////////////////////
+    // command latch 0x90 (ID Read)
+    io0_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
+    io1_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
+    io2_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
+    io3_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
+    io4_pin.set_state(bsp::hal::gpio::PinState::High).unwrap();
+    io5_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
+    io6_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
+    io7_pin.set_state(bsp::hal::gpio::PinState::High).unwrap();
 
-    ////////////////////////////////////////////////////
-    assembler.bind(&mut label_wrap_target);
+    cle_pin.set_state(bsp::hal::gpio::PinState::High).unwrap();
 
-    assembler.bind(&mut label_wrap_source);
+    ceb0_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
+    ceb1_pin.set_state(bsp::hal::gpio::PinState::High).unwrap();
 
-    let program = assembler.assemble_with_wrap(label_wrap_source, label_wrap_target);
+    web_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
+    ale_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
+    reb_pin.set_state(bsp::hal::gpio::PinState::High).unwrap();
+    delay.delay_ms(1);
 
-    // run pio0
-    let (mut pio, sm0, _, _, _) = pac.PIO0.split(&mut pac.RESETS);
-    let installed = pio.install(&program).unwrap();
-    let (int, frac) = (0, 0);
-    let (sm, _, _) = PIOBuilder::from_program(installed)
-        .set_pins(led_pin.id().num, 1) // TODO:ピン設定とRBBのInternal Pulldown
-        .clock_divisor_fixed_point(int, frac)
-        .build(sm0);
-    sm.start();
+    web_pin.set_state(bsp::hal::gpio::PinState::High).unwrap();
+    delay.delay_ms(1);
 
-    // 指定したbitだけ1の値
-    let bit_on = |bit_pos: u32| (0x01u32 << bit_pos);
+    ////////////////////////////////
+    // Address latch 0x00
+    io0_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
+    io1_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
+    io2_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
+    io3_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
+    io4_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
+    io5_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
+    io6_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
+    io7_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
 
-    // RBB以外全部Output
-    let write_pin_dir = bit_on(NandPinAssign::LedPin as u32)
-        | bit_on(NandPinAssign::RebPin as u32)
-        | bit_on(NandPinAssign::WebPin as u32)
-        | bit_on(NandPinAssign::WpbPin as u32)
-        | bit_on(NandPinAssign::AlePin as u32)
-        | bit_on(NandPinAssign::ClePin as u32)
-        | bit_on(NandPinAssign::Ceb1Pin as u32)
-        | bit_on(NandPinAssign::Ceb0Pin as u32)
-        | bit_on(NandPinAssign::Io7Pin as u32)
-        | bit_on(NandPinAssign::Io6Pin as u32)
-        | bit_on(NandPinAssign::Io5Pin as u32)
-        | bit_on(NandPinAssign::Io4Pin as u32)
-        | bit_on(NandPinAssign::Io3Pin as u32)
-        | bit_on(NandPinAssign::Io2Pin as u32)
-        | bit_on(NandPinAssign::Io1Pin as u32)
-        | bit_on(NandPinAssign::Io0Pin as u32);
-    // RBB,IO以外Output
-    let read_pin_dir = bit_on(NandPinAssign::LedPin as u32)
-        | bit_on(NandPinAssign::RebPin as u32)
-        | bit_on(NandPinAssign::WebPin as u32)
-        | bit_on(NandPinAssign::WpbPin as u32)
-        | bit_on(NandPinAssign::AlePin as u32)
-        | bit_on(NandPinAssign::ClePin as u32)
-        | bit_on(NandPinAssign::Ceb1Pin as u32)
-        | bit_on(NandPinAssign::Ceb0Pin as u32);
-    // Assert=/WP, Negate=/CS,/RE,/WE,/CLE,ALE,
-    let init_pin_state = bit_on(NandPinAssign::RebPin as u32)
-        | bit_on(NandPinAssign::WebPin as u32)
-        | bit_on(NandPinAssign::Ceb1Pin as u32)
-        | bit_on(NandPinAssign::Ceb0Pin as u32);
-    // CS0, CS1設定
-    let access_to_cs0 = true;
-    let (en_cs_pin, dis_cs_pin) = if access_to_cs0 {
-        (NandPinAssign::Ceb0Pin as u32, NandPinAssign::Ceb1Pin as u32)
-    } else {
-        (NandPinAssign::Ceb1Pin as u32, NandPinAssign::Ceb0Pin as u32)
-    };
+    cle_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
+
+    ceb0_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
+    ceb1_pin.set_state(bsp::hal::gpio::PinState::High).unwrap();
+
+    web_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
+    ale_pin.set_state(bsp::hal::gpio::PinState::High).unwrap();
+    reb_pin.set_state(bsp::hal::gpio::PinState::High).unwrap();
+    delay.delay_ms(1);
+
+    web_pin.set_state(bsp::hal::gpio::PinState::High).unwrap();
+    delay.delay_ms(1); // TODO: wait t_AR
+
+    ////////////////////////////////
+    // ready for dataread (/RE = Low & IO dir = Input)
+    let io0_pin = io0_pin.into_pull_down_input();
+    let io1_pin = io1_pin.into_pull_down_input();
+    let io2_pin = io2_pin.into_pull_down_input();
+    let io3_pin = io3_pin.into_pull_down_input();
+    let io4_pin = io4_pin.into_pull_down_input();
+    let io5_pin = io5_pin.into_pull_down_input();
+    let io6_pin = io6_pin.into_pull_down_input();
+    let io7_pin = io7_pin.into_pull_down_input();
+
+    cle_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
+
+    ceb0_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
+    ceb1_pin.set_state(bsp::hal::gpio::PinState::High).unwrap();
+
+    web_pin.set_state(bsp::hal::gpio::PinState::High).unwrap();
+    ale_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
+    reb_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
+    delay.delay_ms(1); // TODO: wait t_REA
+
+    //////////////////////////////////////
+    // data read
+    for read_index in 0..5 {
+        let read_data: u8 = {
+            (if io0_pin.is_high().unwrap() {
+                0x01
+            } else {
+                0x00
+            }) | (if io1_pin.is_high().unwrap() {
+                0x02
+            } else {
+                0x00
+            }) | (if io2_pin.is_high().unwrap() {
+                0x04
+            } else {
+                0x00
+            }) | (if io3_pin.is_high().unwrap() {
+                0x08
+            } else {
+                0x00
+            }) | (if io4_pin.is_high().unwrap() {
+                0x10
+            } else {
+                0x00
+            }) | (if io5_pin.is_high().unwrap() {
+                0x20
+            } else {
+                0x00
+            }) | (if io6_pin.is_high().unwrap() {
+                0x40
+            } else {
+                0x00
+            }) | (if io7_pin.is_high().unwrap() {
+                0x80
+            } else {
+                0x00
+            })
+        };
+        info!("data[{}] = {:#02x}", read_index, read_data);
+
+        // next cyc
+        reb_pin.set_state(bsp::hal::gpio::PinState::High).unwrap();
+        delay.delay_ms(1);
+        reb_pin.set_state(bsp::hal::gpio::PinState::Low).unwrap();
+        delay.delay_ms(1); // TODO: wait t_REA
+    }
 
     loop {
-        // pio
-        cortex_m::asm::wfi();
+        led_pin.set_high().unwrap();
+        delay.delay_ms(1000);
+        led_pin.set_low().unwrap();
+        delay.delay_ms(1000);
     }
 }
 
