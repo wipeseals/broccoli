20569832a4f0a042a205e814dc331fbe9d87ecee
wipeseals <wipeseals@gmail.com>
2024-05-07 00:12:17 +0900

    build: bitarray追加

--- misc/setup_python.bat
+++ misc/setup_python.bat
@@ -2,6 +2,7 @@ pip install numpy
 pip install pandas 
 pip install altair 
 pip install jupyter 
+pip install bitarray 
 pip install wavedrom 
 pip install svgwrite 
 pip install rp2040-pio-emulator@git+https://github.com/NathanY3G/rp2040-pio-emulator 
