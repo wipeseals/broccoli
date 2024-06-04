c8b254d5dad1bd9956d5fba03b77574f0af8ac4c
wipeseals <wipeseals@gmail.com>
2024-06-04 23:31:28 +0900

    fix: 型定義修正

--- broccoli-nandio/src/commander.rs
+++ broccoli-nandio/src/commander.rs
@@ -2,28 +2,29 @@
 #![cfg_attr(not(test), no_std)]
 
 use crate::{address::MAX_IC, driver::Driver};
-use bitvec::prelude as bv;
+use bitvec::prelude::*;
 use core::future::Future;
 
+type ValidCsBitArr = BitArr!(for MAX_IC, in u32, Lsb0);
 #[derive(Default)]
 pub struct Commander {
     /// Number of NAND chip
-    pub valid_cs: bv::bitarr![0; MAX_IC],
+    pub valid_cs: ValidCsBitArr,
     /// Number of valid NAND chip
     pub num_cs: usize,
 }
 
 impl Commander {
     /// Communication Setup
-    pub async fn setup_comm(&mut self, driver: &mut impl Driver) -> usize {
+    pub async fn setup(&mut self, driver: &mut impl Driver) -> usize {
         driver.init_pins();
-        self.valid_cs.set_all(false);
+        self.valid_cs.fill(false);
         self.num_cs = 0;
 
         for i in 0..MAX_IC {
             driver.reset(i);
             let (success, _) = driver.read_id_async(i).await;
-            self.valid_cs[i].set(i, success);
+            self.valid_cs.set(i, success);
             self.num_cs += 1;
         }
         self.num_cs
