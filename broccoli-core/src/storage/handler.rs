550ff81f48c182e5d2e56f030a04b9c5b0441664
wipeseals <wipeseals@gmail.com>
2024-09-21 00:11:20 +0900

    feat(wip): Mapping検討中

--- broccoli-core/src/storage/handler.rs
+++ broccoli-core/src/storage/handler.rs
@@ -1,19 +1,25 @@
+use core::mem;
+
 use super::protocol::{StorageHandler, StorageMsgId, StorageRequest, StorageResponse};
 
 /// Buffer Assigning Type for NandStorageHandler
 #[derive(Copy, Clone, Eq, PartialEq)]
 #[cfg_attr(test, derive(Debug))]
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
-pub enum BufferAssign<const LOGICAL_BLOCKS_PER_BUFFER: usize> {
+pub enum CacheDataType {
     /// Empty Buffer
     Empty,
     /// Buffer is being used by Host Data
-    /// lbas: Logical Block Addresses List
-    HostData {
-        lbas: [usize; LOGICAL_BLOCKS_PER_BUFFER],
-    },
+    HostData,
     /// Buffer is being used by Map Data
-    MapData { map_index: usize },
+    MapData,
+}
+
+impl CacheDataType {
+    /// Create a new CacheDataType
+    pub const fn new() -> Self {
+        Self::Empty
+    }
 }
 
 /// Buffer Progress
@@ -23,7 +29,7 @@ pub enum BufferAssign<const LOGICAL_BLOCKS_PER_BUFFER: usize> {
 #[derive(Copy, Clone, Eq, PartialEq)]
 #[cfg_attr(test, derive(Debug))]
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
-pub enum BufferProgress<Error: Eq + PartialEq> {
+pub enum CacheStatus<Error: Copy + Clone + Eq + PartialEq> {
     /// Buffer is Empty (data invalid)
     Initial,
     /// Buffer is being Read (data invalid)
@@ -32,6 +38,8 @@ pub enum BufferProgress<Error: Eq + PartialEq> {
     DecodingAfterRead,
     /// Buffer Read is Complete (data valid)
     ReadComplete { error: Error },
+    /// Buffer data has been changed (data valid)
+    Changed,
     /// Buffer is being Written (data valid, but not yet encoded)
     EncodingBeforeWrite,
     /// Buffer is being Written (data valid, but not yet written)
@@ -40,34 +48,155 @@ pub enum BufferProgress<Error: Eq + PartialEq> {
     WriteComplete { error: Error },
 }
 
-/// Buffer Status
-/// This struct is used to check the status of Buffer
-#[derive(Copy, Clone, Eq, PartialEq)]
-#[cfg_attr(test, derive(Debug))]
-#[cfg_attr(feature = "defmt", derive(defmt::Format))]
-pub struct BufferStatus<
-    const LOGICAL_BLOCKS_PER_BUFFER: usize,
-    Error: Eq + PartialEq,
-    NandAddr: Eq + PartialEq,
-> {
-    /// Physical Address
-    pub addr: Option<NandAddr>,
-    /// Buffer Assigning Type
-    pub assign: BufferAssign<LOGICAL_BLOCKS_PER_BUFFER>,
-    /// Buffer Progress
-    pub progress: BufferProgress<Error>,
+impl<Error: Copy + Clone + Eq + PartialEq> CacheStatus<Error> {
+    /// Create a new CacheStatus
+    pub const fn new() -> Self {
+        Self::Initial
+    }
+
+    /// Check if the buffer is reusable
+    /// 初期状態、読み込み完了、書き込み完了の場合は再利用可能
+    pub fn is_reusable(&self) -> bool {
+        match self {
+            Self::Initial => true,
+            Self::ReadComplete { error: _ } => true,
+            Self::WriteComplete { error: _ } => true,
+            _ => false,
+        }
+    }
+
+    /// Check if the buffer is valid
+    /// 読み込み完了、データ変更、書き込み完了の場合はデータが有効
+    pub fn is_data_unchanged(&self) -> bool {
+        match self {
+            Self::ReadComplete { error: _ } => true,
+            Self::Changed => true,
+            Self::WriteComplete { error: _ } => true,
+            _ => false,
+        }
+    }
+
+    /// Check if the buffer is clean
+    /// 初期状態、読み込み完了、書き込み完了の場合はデータがRAM上で変更されている
+    pub fn is_data_changed(&self) -> bool {
+        match self {
+            Self::Changed => true,
+            Self::EncodingBeforeWrite => true,
+            Self::Writing => true,
+            _ => false,
+        }
+    }
 }
 
-impl<const LOGICAL_BLOCKS_PER_BUFFER: usize, Error: Eq + PartialEq, NandAddr: Eq + PartialEq>
-    BufferStatus<LOGICAL_BLOCKS_PER_BUFFER, Error, NandAddr>
+/// Buffer Status
+/// This struct is used to check the status of Buffer
+#[derive(Eq, PartialEq)]
+#[cfg_attr(test, derive(Debug))]
+#[cfg_attr(feature = "defmt", derive(defmt::Format))]
+pub struct CacheBuffer<
+    LogicalAddr: Copy + Clone + Eq + PartialEq,
+    NandAddr: Copy + Clone + Eq + PartialEq + Copy,
+    Error: Copy + Clone + Eq + PartialEq,
+    const LOGICAL_BLOCK_SIZE: usize,
+> {
+    /// Logical Address
+    pub logical_addr: Option<LogicalAddr>,
+
+    /// Physical Address
+    pub nand_addr: Option<NandAddr>,
+
+    /// Buffer Status
+    pub status: CacheStatus<Error>,
+
+    /// Buffer Type
+    pub buffer_type: CacheDataType,
+
+    /// Buffer Data
+    pub data: [u8; LOGICAL_BLOCK_SIZE],
+}
+
+impl<
+        LogicalAddr: Copy + Clone + Eq + PartialEq,
+        NandAddr: Copy + Clone + Eq + PartialEq,
+        Error: Copy + Clone + Eq + PartialEq,
+        const LOGICAL_BLOCK_SIZE: usize,
+    > CacheBuffer<LogicalAddr, NandAddr, Error, LOGICAL_BLOCK_SIZE>
 {
-    /// Create a new BufferStatus
+    /// Create a new CacheBuffer
     pub const fn new() -> Self {
         Self {
-            addr: None,
-            assign: BufferAssign::Empty,
-            progress: BufferProgress::Initial,
+            logical_addr: None,
+            nand_addr: None,
+            status: CacheStatus::new(),
+            buffer_type: CacheDataType::new(),
+            data: [0; LOGICAL_BLOCK_SIZE],
         }
+    }
+}
+
+pub struct NandAddressMapper<
+    /// Logical Address
+    LogicalAddr: Copy + Clone + Eq + PartialEq,
+    /// NAND Address
+    NandAddr: Copy + Clone + Eq + PartialEq,
+    /// page bytes (data + spare bytes)
+    const PAGE_TOTAL_SIZE: usize,
+    /// page bytes (spare bytes)
+    const PAGE_META_SIZE: usize,
+    /// Number of pages per block
+    const NUM_PAGES_PER_BLOCK: usize,
+    /// Number of blocks per IC
+    const NUM_BLOCKS_PER_IC: usize,
+    /// Number of ICs
+    const NUM_IC: usize,
+    /// Number of blocks per IC
+    const LOGICAL_BLOCK_SIZE: usize,
+    /// Number of logical addresses
+    const NUM_LOGICAL_ADDR: usize,
+    /// Number of blocks per IC
+    const CACHE_BUFFFER_N: usize,
+> {
+    // pub map_page_address_table: [Option<NandAddr>; NUM_LOGICAL_ADDR/]
+}
+
+impl<
+        LogicalAddr: Copy + Clone + Eq + PartialEq,
+        NandAddr: Copy + Clone + Eq + PartialEq,
+        const PAGE_TOTAL_SIZE: usize,
+        const PAGE_META_SIZE: usize,
+        const NUM_PAGES_PER_BLOCK: usize,
+        const NUM_BLOCKS_PER_IC: usize,
+        const NUM_IC: usize,
+        const LOGICAL_BLOCK_SIZE: usize,
+        const NUM_LOGICAL_ADDR: usize,
+        const CACHE_BUFFFER_N: usize,
+    >
+    NandAddressMapper<
+        LogicalAddr,
+        NandAddr,
+        PAGE_TOTAL_SIZE,
+        PAGE_META_SIZE,
+        NUM_PAGES_PER_BLOCK,
+        NUM_BLOCKS_PER_IC,
+        NUM_IC,
+        LOGICAL_BLOCK_SIZE,
+        NUM_LOGICAL_ADDR,
+        CACHE_BUFFFER_N,
+    >
+{
+    /// NAND Pageのうち、データ部分のサイズ
+    pub const fn page_data_size(&self) -> usize {
+        PAGE_TOTAL_SIZE - PAGE_META_SIZE
+    }
+    /// NAND PageあたりのNandAddrのエントリ数
+    pub const fn map_entries_per_page(&self) -> usize {
+        (PAGE_TOTAL_SIZE - PAGE_META_SIZE) / mem::size_of::<NandAddr>()
+    }
+
+    /// NAND PageあたりのNandAddrエントリが示せる容量
+    pub const fn map_capacity_per_page(&self) -> usize {
+        (PAGE_TOTAL_SIZE - PAGE_META_SIZE) / mem::size_of::<NandAddr>()
+            * ((PAGE_TOTAL_SIZE - PAGE_META_SIZE) / LOGICAL_BLOCK_SIZE) // TODO: 関数分割したいがエラーになる
     }
 }
 
@@ -75,29 +204,19 @@ impl<const LOGICAL_BLOCKS_PER_BUFFER: usize, Error: Eq + PartialEq, NandAddr: Eq
 /// Buffer Size == Nand Page Size
 /// Logical Block Size <= Nand Page Size
 pub struct NandStorageHandler<
+    LogicalAddr: Copy + Clone + Eq + PartialEq,
+    NandAddr: Copy + Clone + Eq + PartialEq,
+    const PAGE_TOTAL_SIZE: usize,
+    const PAGE_META_SIZE: usize,
+    const NUM_PAGES_PER_BLOCK: usize,
+    const NUM_BLOCKS_PER_IC: usize,
+    const NUM_IC: usize,
     const LOGICAL_BLOCK_SIZE: usize,
-    const NAND_PAGE_TOTAL_SIZE: usize,
-    const NAND_PAGE_METADATA_SIZE: usize,
+    const NUM_LOGICAL_ADDR: usize,
+    const CACHE_BUFFFER_N: usize,
     const READ_BUFFER_N: usize,
     const WRITE_BUFFER_N: usize,
 > {
-    /// Internal Read Buffer Status
-    internal_read_status: BufferStatus<LOGICAL_BLOCK_SIZE, u8, usize>,
-    /// Internal Write Buffer Status
-    internal_write_status: BufferStatus<LOGICAL_BLOCK_SIZE, u8, usize>,
-    /// Read Buffer Status (NAND_PAGE_SIZE * READ_BUFFER_N)
-    host_read_statuses: [BufferStatus<LOGICAL_BLOCK_SIZE, u8, usize>; READ_BUFFER_N],
-    /// Write Buffer Status (NAND_PAGE_SIZE * WRITE_BUFFER_N)
-    host_write_statuses: [BufferStatus<LOGICAL_BLOCK_SIZE, u8, usize>; WRITE_BUFFER_N],
-
-    /// Internal Read Buffer (NAND_PAGE_SIZE)
-    internal_read_buffer: [u8; NAND_PAGE_TOTAL_SIZE],
-    /// Internal Write Buffer (NAND_PAGE_SIZE)
-    internal_write_buffer: [u8; NAND_PAGE_TOTAL_SIZE],
-    /// Read Buffer (NAND_PAGE_SIZE * READ_BUFFER_N)
-    host_read_buffers: [[u8; NAND_PAGE_TOTAL_SIZE]; READ_BUFFER_N],
-    /// Write Buffer (NAND_PAGE_SIZE * WRITE_BUFFER_N)
-    host_write_buffers: [[u8; NAND_PAGE_TOTAL_SIZE]; WRITE_BUFFER_N],
     // TODO: Add NAND Controller
     // TODO: Add NAND Map
     // TODO: Add NAND Block Assignment
@@ -145,10 +264,10 @@ impl<
             panic!("NAND_PAGE_SIZE must be larger than LOGICAL_BLOCK_SIZE");
         }
         Self {
-            internal_read_status: BufferStatus::new(),
-            internal_write_status: BufferStatus::new(),
-            host_read_statuses: [BufferStatus::new(); READ_BUFFER_N],
-            host_write_statuses: [BufferStatus::new(); WRITE_BUFFER_N],
+            internal_read_status: CacheBuffer::new(),
+            internal_write_status: CacheBuffer::new(),
+            host_read_statuses: [CacheBuffer::new(); READ_BUFFER_N],
+            host_write_statuses: [CacheBuffer::new(); WRITE_BUFFER_N],
             internal_read_buffer: [0; NAND_PAGE_TOTAL_SIZE],
             internal_write_buffer: [0; NAND_PAGE_TOTAL_SIZE],
             host_read_buffers: [[0; NAND_PAGE_TOTAL_SIZE]; READ_BUFFER_N],
